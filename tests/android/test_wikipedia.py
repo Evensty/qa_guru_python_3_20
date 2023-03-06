d1e1db9554950a11b1eecb4a69e715ec38c07308
evensty <maxim.veselov11@gmail.com>
2023-03-07 00:58:39 +0300

    add one more mobile test

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -4,7 +4,7 @@ from selene.support.shared import browser
 from allure import step
 
 
-def test_search():
+def test_search_browserstack():
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type(
@@ -15,3 +15,11 @@ def test_search():
         browser.all(
             (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
         ).should(have.size_greater_than(0))
+
+
+def test_search_python():
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).send_keys("python")
+    with step('Verify content found'):
+        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).should(have.size_greater_than(0))
